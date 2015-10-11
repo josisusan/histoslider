20fd8ec73e7ffc9c5028a0d81d0917b8ffc4def6
Sam Hogg <sam@samhogg.com>
2015-10-11 09:51:18 +0900

    Initial pages cut

---
+++ main.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Histoslider from '../lib/components/Histoslider'
+
+import arrays from 'd3-arrays'
+
+const main = document.getElementById('main')
+
+class App extends Component {
+
+  histogramChanged (newSelection) {
+    this.setState({
+      selection: newSelection
+    })
+  }
+
+  constructor () {
+    super()
+    this.state = {
+      selection: [4, 2]
+    }
+  }
+
+  changeX (e) {
+    this.setState({
+      selection: [+e.target.value, this.state.selection[1]]
+    })
+  }
+
+  changeY (e) {
+    this.setState({
+      selection: [this.state.selection[0], +e.target.value]
+    })
+  }
+
+  render () {
+    const data = [1, 1, 1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 5, 5, 6, 7, 7, 7, 7, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 3, 2, 3, 3, 3, 3, 3]
+    return (
+      <div>
+        <div style={{ margin: '0 0 1rem 0' }}>
+          <p>
+            <b>Selection</b>
+          </p>
+          <p>{ this.state.selection ? JSON.stringify(this.state.selection) : 'None' }</p>
+        </div>
+        <div className='demo-wrapper'>
+          <Histoslider
+            selection={this.state.selection}
+            padding={20}
+            width={500}
+            height={200}
+            data={data}
+            onChange={this.histogramChanged.bind(this)}
+          />
+          <Histoslider
+            selection={this.state.selection}
+            padding={20}
+            width={500}
+            height={200}
+            bucketSize={3}
+            selectionColor='purple'
+            data={data}
+            onChange={this.histogramChanged.bind(this)}
+          />
+          <Histoslider
+            selection={this.state.selection}
+            padding={20}
+            width={500}
+            height={200}
+            selectionColor='orange'
+            bucketSize={6}
+            data={data}
+            onChange={this.histogramChanged.bind(this)}
+          />
+          <Histoslider
+            selection={this.state.selection}
+            padding={15}
+            width={500}
+            height={200}
+            selectionColor='steelblue'
+            bucketSize={2.5}
+            data={data}
+            onChange={this.histogramChanged.bind(this)}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(React.createElement(App), main)
