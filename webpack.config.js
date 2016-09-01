e5ea3fd5d50fee7a2e3db9cf1ae5201160cd6bb0
josisusan <josisusan@gmail.com>
2016-09-01 22:29:33 +0545

    Add babel-loader and presets.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ module.exports = {
       {
         test: /\.js?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
       }
     ]
   }
